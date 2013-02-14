8b27236f539e7b6cb20c1b58779ac16107edad6f
Yao Li <yao@freelab.(none)>
2013-02-14 13:26:18 -0500

    update header files for GPU platform parallel GBH segmentation and ./gbh.cu(104): Error: Formal parameter space overflowed (256 bytes max) in function __global__ void gb(universe *mess, image<float> *smooth_r[], image<float> *smooth_g[], image<float> *smooth_b[], int width, int height, float c, edge *edges_remain0[], edge *edges_remain1[], edge *edges_remain2[], edge *edges_remain3[], edge *edges_remain4[], edge *edges_remain5[], edge *edges_remain6[], edge *edges_remain7[], edge *edges0, edge *edges1, edge *edges2, edge *edges3, edge *edges4, edge *edges5, edge *edges6, edge *edges7, float *threshold0, float *threshold1, float *threshold2, float *threshold3, float *threshold4, float *threshold5, float *threshold6, float *threshold7, universe_s *u0, universe_s *u1, universe_s *u2, universe_s *u3, universe_s *u4, universe_s *u5, universe_s *u6, universe_s *u7 ) {

--- segment-graph-s.h
+++ segment-graph-s.h
@@ -44,86 +44,23 @@ bool operator>(const edge &a, const edge &b) {
 }
 
 template<class T>
-__host__ __device__ void quicksort(T a[], const int& leftarg, const int& rightarg)
+__host__ __device__ void bubble_sort(T *items, int count)
 {
-  if (leftarg < rightarg) {
+  T t;
 
-    T pivotvalue = a[leftarg];
-    int left = leftarg - 1;
-    int right = rightarg + 1;
-
-  for(;;) {
-
-    while (a[--right] > pivotvalue);
-    while (a[++left] < pivotvalue);
-
-    if (left >= right) break;
-
-    T temp = a[right];
-    a[right] = a[left];
-    a[left] = temp;
-  }
-
-  int pivot = right;
-  quicksort(a, leftarg, pivot);
-  quicksort(a, pivot + 1, rightarg);
-  }
+  for(int a=1; a<count; a++)
+    for(int b=count-1; b>=a; b--)
+      if(items[b-1] > items[b]) {
+        t = items[b-1];
+        items[b-1] = items[b];
+        items[b] = t;
+      }
 }
 
 __host__ __device__ int back_push(edge *edges_remain[], edge *pedge, int cur_it) {
   edges_remain[cur_it] = pedge;
   ++cur_it;
   return cur_it;
-}
-
-template<class TYPE>
-void algo_mergesort(size_t n, TYPE array[], TYPE *temp = 0)
-{
-	TYPE *a2[2], *a, *b;
-	int curr, shift;
-
-	a2[0] = array;
-	a2[1] = temp? temp : new TYPE[n];
-	for (curr = 0, shift = 0; (1ul<<shift) < n; ++shift) {
-		a = a2[curr]; b = a2[1-curr];
-		if (shift == 0) {
-			TYPE *p = b, *i, *eb = a + n;
-			for (i = a; i < eb; i += 2) {
-				if (i == eb - 1) *p++ = *i;
-				else {
-					if (*(i+1) < *i) {
-						*p++ = *(i+1); *p++ = *i;
-					} else {
-						*p++ = *i; *p++ = *(i+1);
-					}
-				}
-			}
-		} else {
-			size_t i, step = 1ul<<shift;
-			for (i = 0; i < n; i += step<<1) {
-				TYPE *p, *j, *k, *ea, *eb;
-				if (n < i + step) {
-					ea = a + n; eb = a;
-				} else {
-					ea = a + i + step;
-					eb = a + (n < i + (step<<1)? n : i + (step<<1));
-				}
-				j = a + i; k = a + i + step; p = b + i;
-				while (j < ea && k < eb) {
-					if (*j < *k) *p++ = *j++;
-					else *p++ = *k++;
-				}
-				while (j < ea) *p++ = *j++;
-				while (k < eb) *p++ = *k++;
-			}
-		}
-		curr = 1 - curr;
-	}
-	if (curr == 1) {
-		TYPE *p = a2[0], *i = a2[1], *eb = array + n;
-		for (; p < eb; ++i) *p++ = *i;
-	}
-	if (temp == 0) delete[] a2[1];
 }
 
 /*
@@ -146,7 +83,7 @@ __host__ __device__
   int cur_it = 0; // current available iterator
 
   // sort edges by weight
-  quicksort<edge>(edges, 0, num_edges - 1);
+  bubble_sort<edge>(edges, num_edges);
 
   // make a disjoint-set forest
 //  universe_s *u = new universe_s(num_vertices);
